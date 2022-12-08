4eda47d235831ab4e1ec6f1ac1b69c482c6caa88
Andrew Campi <63204545+andrewcampi@users.noreply.github.com>
2022-12-08 08:54:07 -0500

    Add files via upload

---
+++ maze/maze/GenerationType.java
@@ -0,0 +1,11 @@
+package com.mygdx.maze.maze;
+
+/**
+ * The GenerationType enum represents the generation algorithms
+ * provides by the game to the player.
+ */
+public enum GenerationType
+{
+	ALDOUS_BRODER,
+	RECURSIVE_BACKTRACK;
+}
